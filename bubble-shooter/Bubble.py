c4abca54541d83e10b1c4d5f7b5acafaa69feb18
pooneh <poonehshooshtari@gmail.com>
2018-06-07 00:00:26 -0600

    Added docstring

--- bubble-shooter/Bubble.py
+++ bubble-shooter/Bubble.py
@@ -42,12 +42,17 @@ class Bubble(object):
 
 		# create_oval takes two pairs of coordinates, top_left and bottom_right
 		self.Bubble_index = self.tk_pic.create_oval(self.x0, self.y0, self.x1 , self.y1, outline = "grey", fill = self.color, width = 2)                                        
+		# every Bubble has a unique index in the instance of the TKpic object. this index is stored in a dictionary
+		# defined in the TKPICTURE class
 		self.tk_pic.shapesBubble[self.Bubble_index] = self
 
 
 	
-	# calculating velocity in the updated direction 
+	
 	def fire_Bubble(self, vel, firing_angle):
+		"""
+		calculating velocity in the updated direction based on the firing line angle is passed
+		"""
 
 		vec = ang_to_vec(firing_angle)
 		self.Bubble_vel = [vel*vec[0], vel*vec[1]]
@@ -56,6 +61,9 @@ class Bubble(object):
 	
 	
 	def update_Bubble(self):
+		"""
+		This function is called every frame in order to move the bubble by adding velocity to the current position.
+		"""
 		
 		self.Bubble_initial_pos[0] += self.Bubble_vel[0]
 		self.Bubble_initial_pos[1] += self.Bubble_vel[1]
@@ -71,6 +79,10 @@ class Bubble(object):
 
 
 	def check_boundary(self):
+		"""
+		calculate the distance between the center of the bubble to the vertical boundaries
+		so if a bubble hits a boundary the velocity will be reversed and the bubble will bounced back
+		"""
 
 		if self.Bubble_initial_pos[0] <= self.Bubble_radius or self.Bubble_initial_pos[0] >= self.tk_pic.width - self.Bubble_radius:
 			self.Bubble_vel[0] = -self.Bubble_vel[0]
@@ -78,6 +90,9 @@ class Bubble(object):
 	
 
 	def is_stuck(self, Set, list_of_grids):
+		"""
+		checking if the bubble hits the top boundary or checking against other bubbles in a set
+		"""
 	
 		if self.Bubble_initial_pos[1] <= self.Bubble_radius:
 			self.min_dist_grid(list_of_grids, self.Bubble_initial_pos)
@@ -97,6 +112,10 @@ class Bubble(object):
 
 	
 	def make_grid(self):
+		"""
+		make grid returns the positions of each possible bubble placement on the grid
+		the odd rows are shifted 
+		"""
 
 		init_grid = (self.grid_width//2, self.grid_height//2)
 		grid_list = []
@@ -113,7 +132,10 @@ class Bubble(object):
 
 
 
-	def min_dist_grid(self, list_of_grids, self_pos): 
+	def min_dist_grid(self, list_of_grids, self_pos):
+		"""
+		finding the closest position in the grid to the center of the bubble after hitting boundary or another bubble
+		"""
 		
 		distance = []
 		cp_list_of_grids = list(list_of_grids)
@@ -140,14 +162,17 @@ class Bubble(object):
   	
 	
 	def build_adj_dict(self, Set):
-
+		"""
+		this function takes a set parameter which includes all the bubbles already shooted and check against 
+		tne new bubble for adjacency, if this is the case. it puts the adjacent bubble inside the adjacency dictionary 
+		"""
   		if len(Set):
 
   			for bubble in Set:	
   				
   				distance = dist(bubble.Bubble_last_pos, self.Bubble_last_pos)
   				if  distance <= (bubble.Bubble_radius * 1.2 + self.Bubble_radius * 1.2):
-  					
+  					# add edge between new bubble and existing bubble
   					if bubble.color not in self.adj_dict.keys():
 						self.adj_dict.setdefault(bubble.color,[]).append(bubble)
 					else:
@@ -165,10 +190,12 @@ class Bubble(object):
 		return self.adj_dict
 
 
-
-	
-
 	def get_bubble_chain(self, same_color_chain_list):
+		"""
+		after shooting the current bubble we need to check the same color chain list and basically do a DFS
+		chdecking the adjacent dictionary of the current bubble and go through all the neighbors and make a list 
+		of the neighbors with the same color(DFS). (note: it is recursively called on the neighbors of the adjacent neighbor)
+		"""
 
 		if self.color in self.adj_dict.keys():
 			for bubble in self.adj_dict[self.color]:
@@ -179,10 +206,12 @@ class Bubble(object):
 
 
 
-	
-	
-
 	def bubble_chain_delete(self, same_color_chain_list):
+		"""
+		iterating through adjacent dictioanry of the bubble and if the adjacent bubble is in the 
+		same color chain list we remove the adjacent bubble from adjacent dictionary and continue the process 
+		recursively.
+		"""
 
 		for bubble in self.adj_dict[self.color]:
 			if bubble in same_color_chain_list:
